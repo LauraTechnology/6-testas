da6c71487204ca728c89070629751fedf845c156
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-11 13:47:28 +0300

    bandymas1

---
+++ 3-vidurkis-worst.js
@@ -0,0 +1,61 @@
+const data = [
+    {
+        name: 'Jonas',
+        marks: [10, 2, 8, 4, 6]
+    },
+    {
+        name: 'Maryte',
+        marks: [3, 7, 9, 5]
+    },
+    {
+        name: 'Petras',
+        marks: [7, 7, 7]
+    },
+    {
+        name: 'Ona',
+        marks: [10, 9, 8, 7, 6]
+    },
+]
+
+// 3 - Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi prasciausia pazymiu vidurki
+
+// name: 'Jonas';
+
+if (marksJonas) {
+    mark = a, b, c, d, e
+
+    a = 10;
+    b = 2;
+    c = 8;
+    d = 4;
+    e = 6;
+    
+} else {
+    var suma;
+    
+    suma = a + b + c + d + e;
+    
+    var vidurkis;
+    
+    vidurkis = suma / 5;
+    
+    
+} if (marksMaryte) { 
+
+} else {
+
+} if (marksPetras) {
+
+} else {
+
+} if (marksOna) {
+
+} else {
+
+}
+
+
+
+
+
+console.log(vidurkis)
