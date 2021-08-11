a8b60234b53b1c93a3b0a9b6380367e6972f5b1b
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-11 14:37:41 +0300

    issprestas

--- 1-worst.js
+++ 1-worst.js
@@ -18,3 +18,15 @@ const data = [
 ]
 
 // 1 - Isspausdinti mokinio varda ir pazymi, kuris turi prasciausia pazymi
+
+
+
+// If Jonas marks is 2,4,6,8,10 : lowest is 2
+
+let lowest = 2;
+
+if (lowest >=2) {
+    console.log('Jonas');
+}
+
+
